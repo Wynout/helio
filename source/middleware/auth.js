5541ba0614d1c76bb66d46cd43f2fbcf42a0f85e
Wynout van der Veer <wynout@outlook.com>
2014-02-05 18:47:15 +0100

    Fix coding style

--- source/middleware/auth.js
+++ source/middleware/auth.js
@@ -1,48 +1,53 @@
-var express = require('express');
-var moment = require('moment');
-var crypto = require('crypto');
-
-var TOKEN_TTL_MINUTES = 60;
-var AUTH_SIGN_KEY = '95810db3f765480999a8d5089b0815bd4b55e831';
+var AUTH_SIGN_KEY     = '95810db3f765480999a8d5089b0815bd4b55e831',
+	crypto            = require('crypto'),
+	express           = require('express'),
+	moment            = require('moment'),
+	TOKEN_TTL_MINUTES = 60;
 
 function createToken(req, res, next) {
-	var username = req.user.username;
-	var timespamp = moment();
-	var message = username + ';' + timespamp.valueOf();
-	var hmac = crypto.createHmac('sha1', AUTH_SIGN_KEY).update(message).digest('hex');
-	var token = username + ';' + timespamp.valueOf() + ';' + hmac;
-	var tokenBase64 = new Buffer(token).toString('base64');
+
+	var username    = req.user.username,
+		timestamp   = moment(),
+		message     = username + ';' + timestamp.valueOf(),
+		hmac        = crypto.createHmac('sha1', AUTH_SIGN_KEY).update(message).digest('hex'),
+		token       = username + ';' + timestamp.valueOf() + ';' + hmac,
+		tokenBase64 = new Buffer(token).toString('base64');
 
 	req.token = tokenBase64;
-
 	next();
 }
 
-function validateToken (req, res, next) {
+function validateToken(req, res, next) {
+
 	var basic = express.basicAuth(hmacAuthentication);
 	return basic(req, res, next);
 
 	function hmacAuthentication(user, password) {
-		var token = new Buffer(password, 'base64').toString();
-		var parsed = token.split(';');
+
+		var token  = new Buffer(password, 'base64').toString(),
+			parsed = token.split(';');
 
 		if (parsed.length !== 3) {
 			return false;
 		}
 
-		var username = parsed[0], timespamp = parsed[1], recievedHmac = parsed[2];
-		var message = username + ';' + timespamp;
-		var computedHmac = crypto.createHmac('sha1', AUTH_SIGN_KEY).update(message).digest('hex');
+		var username     = parsed[0],
+			timestamp    = parsed[1],
+			receivedHmac = parsed[2],
+			message      = username + ';' + timestamp,
+			computedHmac = crypto.createHmac('sha1', AUTH_SIGN_KEY).update(message).digest('hex');
 
-		if (recievedHmac !== computedHmac) {
+		if (receivedHmac!==computedHmac) {
 			return false;
 		}
 
-		var currentTimespamp = moment(), recievedTimespamp = moment(+timespamp);
-		if (recievedTimespamp.diff(currentTimespamp, 'minutes') > TOKEN_TTL_MINUTES) {
+		var currentTimestamp = moment(),
+			receivedTimespamp = moment(+timestamp);
+
+		if (receivedTimespamp.diff(currentTimestamp, 'minutes') > TOKEN_TTL_MINUTES) {
+
 			return false;
 		}
-
 		return true;
 	}
 }
