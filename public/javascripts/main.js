de939d2957e3acadf98af5352b2dedb0d026e626
Wynout van der Veer <wynout@gmail.com>
2014-02-15 11:48:19 +0100

    Cleanup requirejs main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,38 +2,23 @@ require.config({
 
     baseURL: 'public/javascripts/components',
 
-    shim: {
-        // 'jquery.mobile-config': ['jquery'],
-        // 'jquery.mobile': ['jquery', 'jquery.mobile-config']
-        // 'backbone.validation': {
-        //     deps: ['./views/backboneValidation']
-        // }
-
-        // 'backbone.validation': {
-        //     deps: ['./components/backbone-validation/dist/backbone-validation']
-        // }
-    },
     paths: {
-        jquery: "./components/jquery/jquery",
-        underscore: "./components/underscore-amd/underscore",
-        backbone: "./components/backbone-amd/backbone",
-        "backbone.wreqr": "./components/backbone.wreqr/lib/amd/backbone.wreqr",
-        "backbone.babysitter": "./components/backbone.babysitter/lib/amd/backbone.babysitter",
+        jquery               : './components/jquery/jquery',
+        underscore           : './components/underscore-amd/underscore',
+        backbone             : './components/backbone-amd/backbone',
+        'backbone.wreqr'     : './components/backbone.wreqr/lib/amd/backbone.wreqr',
+        'backbone.babysitter': './components/backbone.babysitter/lib/amd/backbone.babysitter',
         'backbone.validation.callbacks': './views/backboneValidation',
-
-
-        // test
         'backbone.validation': './components/backbone-validation/dist/backbone-validation-amd',
 
-        marionette: "./components/marionette/lib/core/amd/backbone.marionette",
-        hbs           : './components/require-handlebars-plugin/hbs',
-        // underscore    : './components/require-handlebars-plugin/hbs/underscore',
-        handlebars    : './components/require-handlebars-plugin/Handlebars',
-        i18nprecompile: './components/require-handlebars-plugin/hbs/i18nprecompile',
-        json2         : './components/require-handlebars-plugin/hbs/json2',
+        marionette            : './components/marionette/lib/core/amd/backbone.marionette',
+        hbs                   : './components/require-handlebars-plugin/hbs',
+        handlebars            : './components/require-handlebars-plugin/Handlebars',
+        i18nprecompile        : './components/require-handlebars-plugin/hbs/i18nprecompile',
+        json2                 : './components/require-handlebars-plugin/hbs/json2',
 
         'jquery.mobile-config': 'config/jquery.mobile-config',
-        'jquery.mobile': './components/jquery-mobile/dist/jquery.mobile' // build by grunt
+        'jquery.mobile'       : './components/jquery-mobile/dist/jquery.mobile' // build by grunt
 
     },
     hbs: {
--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main-81edf09a532887594adc00b7cecdad36.js',
+	js: '/build/main-fe0fd2cdb3d6822dc9b5f144f062aac6.js',
 	css: '/build/main-8eb67c9a63496fd2af867c0f9213d1f3.css'
 };
