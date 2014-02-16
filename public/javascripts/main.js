b500a619f6face3a3f206f9bcb9ade3fa45e7009
Wynout van der Veer <wynout@gmail.com>
2014-02-16 11:00:03 +0100

    Rename components to bower_components. Be more descriptive.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,24 +5,24 @@
 */
 require.config({
 
-    baseURL: 'public/javascripts/components',
+    baseURL: 'public/javascripts/bower_components',
 
     paths: {
-        jquery               : './components/jquery/jquery',
-        underscore           : './components/underscore-amd/underscore',
-        backbone             : './components/backbone-amd/backbone',
-        'backbone.wreqr'     : './components/backbone.wreqr/lib/amd/backbone.wreqr',
-        'backbone.babysitter': './components/backbone.babysitter/lib/amd/backbone.babysitter',
-        'backbone.validation': './components/backbone-validation/dist/backbone-validation-amd',
+        jquery               : './bower_components/jquery/jquery',
+        underscore           : './bower_components/underscore-amd/underscore',
+        backbone             : './bower_components/backbone-amd/backbone',
+        'backbone.wreqr'     : './bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
+        'backbone.babysitter': './bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+        'backbone.validation': './bower_components/backbone-validation/dist/backbone-validation-amd',
 
-        marionette            : './components/marionette/lib/core/amd/backbone.marionette',
-        hbs                   : './components/require-handlebars-plugin/hbs',
-        handlebars            : './components/require-handlebars-plugin/Handlebars',
-        i18nprecompile        : './components/require-handlebars-plugin/hbs/i18nprecompile',
-        json2                 : './components/require-handlebars-plugin/hbs/json2',
+        marionette            : './bower_components/marionette/lib/core/amd/backbone.marionette',
+        hbs                   : './bower_components/require-handlebars-plugin/hbs',
+        handlebars            : './bower_components/require-handlebars-plugin/Handlebars',
+        i18nprecompile        : './bower_components/require-handlebars-plugin/hbs/i18nprecompile',
+        json2                 : './bower_components/require-handlebars-plugin/hbs/json2',
 
         'jquery.mobile-config': 'config/jquery.mobile-config',
-        'jquery.mobile'       : './components/jquery-mobile/dist/jquery.mobile' // built by grunt
+        'jquery.mobile'       : './bower_components/jquery-mobile/dist/jquery.mobile' // built by grunt
 
     },
     hbs: {
