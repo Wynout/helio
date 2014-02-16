2f882881dbecaf6beefa59e1be0792616b57fae4
Wynout van der Veer <wynout@gmail.com>
2014-02-16 09:41:41 +0100

    Remove button icons login form

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main-fe0fd2cdb3d6822dc9b5f144f062aac6.js',
+	js: '/build/main-f1ada86268dac3779fe8ed138bb15433.js',
 	css: '/build/main-8eb67c9a63496fd2af867c0f9213d1f3.css'
 };
