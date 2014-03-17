0b2e9fe30f1070769b8cd0457ca56c6f83bdb51d
Wynout van der Veer <wynout@gmail.com>
2014-03-17 07:58:15 +0100

    Add i18n to navigation bar

--- public/javascripts/nls/navBar.js
+++ public/javascripts/nls/navBar.js
@@ -1,6 +1,16 @@
 define({
     root: {
-
+        screenReaders: {
+            toggleSidebar: 'Toggle navigation',
+            toggleSettings: 'Toggle settings'
+        },
+        placeholders: {
+            search: 'Search...'
+        },
+        items: {
+            settings: 'Settings',
+            signout: 'Sign out'
+        }
     },
     'nl-nl': true
 });
--- public/javascripts/nls/nl-nl/navBar.js
+++ public/javascripts/nls/nl-nl/navBar.js
@@ -1,3 +1,13 @@
 define({
-
+    screenReaders: {
+        toggleSidebar: 'Wisselknop navigatie',
+        toggleSettings: 'Wisselknop instellingen'
+    },
+    placeholders: {
+        search: 'Zoeken...'
+    },
+    items: {
+        settings: 'Instellingen',
+        signout: 'Uitloggen'
+    }
 });
